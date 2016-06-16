5ce372a66faf27156718b036520bd369859e7629
ShaB <shab@cloud66.com>
2016-06-16 20:22:06 +0100

    demo changes

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"io"
-	"net/http"
+    "io"
+    "io/ioutil"
+    "net/http"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world again?!")
+    io.WriteString(w, "Hello world again!\n")
+    resp, err := http.Get("http://web.cloud66.local/counts")
+    if err != nil {
+        // handle error
+        io.WriteString(w, "Count from web.cloud66.local is: <<unavailable>>")
+        return
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        // handle error
+        io.WriteString(w, "Count from web.cloud66.local is: <<unavailable>>")
+        return
+    }
+    io.WriteString(w, "Count from web.cloud66.local is: "+string(body))
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", nil)
+    http.HandleFunc("/", hello)
+    http.ListenAndServe(":8000", nil)
 }
